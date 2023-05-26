89823fac17da2a5973d492eb6a2e1d80288bd628
MariRionda <maririonda@gmail.com>
2023-05-26 20:07:00 -0300

    PDF de solicitud de siniestro listo

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, { isServer }) => {
+    // Configuración específica para el entorno del servidor (Node.js)
+    if (isServer) {
+      // No realizar ninguna configuración adicional
+      return config;
+    }
 
-module.exports = nextConfig
+    // Configuración específica para el entorno del cliente (navegador)
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-pdf$': 'react-pdf/dist/esm/entry.webpack.js'
+    };
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
+
--- client/src/pages/requests/pdf/[id]/index.tsx
+++ client/src/pages/requests/pdf/[id]/index.tsx
@@ -1,12 +1,20 @@
 import { GetServerSideProps } from "next";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import { getRequest } from "../../../../store/Slices/requestsSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getRequestById } from "../../functions/functions";
 import Loading from "../../../../components/Loading/loading";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
-import { FormButton, SmallBtn } from "../../../../components/Buttons";
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(() => import("@react-pdf/renderer").then((module) => module.PDFViewer), {
+  ssr: false,
+});
+const PDFDownloadLink = dynamic(() => import("@react-pdf/renderer").then((module) => module.PDFDownloadLink), {
+  ssr: false,
+});
+
 import PDFDetail from "../../components/requestDetailPDF";
+import { FormButton } from "../../../../components/Buttons";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { params } = context;
@@ -28,45 +36,50 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const RequestDetailPDF = ({ id }: { id: string }) => {
   const dispatch = useAppDispatch();
-
-  const request = (useAppSelector(getRequest));
+  const request = useAppSelector(getRequest);
+  const [isClient, setIsClient] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
+    setIsClient(true);
     getRequestById(dispatch, id);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Actualizar el ancho de la ventana en el cambio de tamaño
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Obtener el ancho inicial de la ventana
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
-  return (
-    <div>
-      {!request ? (
-        <Loading />
-      ) : (
-        <div>
-          <div className="d-none d-lg-block">
-            <PDFViewer style={{ width: "100%", height: "95vh" }}>
-              <PDFDetail 
-                request={request.request}
-                deceased={request.deceased}
-              />
-            </PDFViewer>
-          </div>
-          <div className="d-lg-none">
-            <PDFDownloadLink
-              style={{ textDecoration: "none" }}
-              document={<PDFDetail
-                request={request.request}
-                deceased={request.deceased}
-              />}
-              fileName={
-                "Detalle de Solicitud de Siniestro - " +
-                request.deceased.name
-              }
-            >
-              <FormButton title={"Descargar PDF"} />
-            </PDFDownloadLink>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  const renderContent = () => {
+    if (windowWidth >= 700) {
+      return (
+        <PDFViewer style={{ width: "100%", height: "95vh" }}>
+          <PDFDetail request={request.request} deceased={request.deceased} />
+        </PDFViewer>
+      );
+    } else {
+      return (
+        <PDFDownloadLink
+          style={{ textDecoration: "none" }}
+          document={<PDFDetail request={request.request} deceased={request.deceased} />}
+          fileName={"Detalle de Solicitud de Siniestro - " + request.deceased.name}
+        >
+          <FormButton title={"Descargar PDF"} />
+        </PDFDownloadLink>
+      );
+    }
+  };
+
+  return <div>{!request ? <Loading /> : <div>{isClient && renderContent()}</div>}</div>;
 };
+
 export default RequestDetailPDF;
+
+
