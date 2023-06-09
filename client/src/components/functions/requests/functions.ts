eb20553c8537d46bd23658497893188888da12c9
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2023-06-09 16:25:09 -0300

    Update functions.ts

--- client/src/components/functions/requests/functions.ts
+++ client/src/components/functions/requests/functions.ts
@@ -1,6 +1,8 @@
+import Swal from "sweetalert2"
 import { getDeceasedByRequestIdApi } from "../../../services/deceasedApi"
-import { getAllRequestsApi, getRequestsByIdApi } from "../../../services/requestApi"
+import { deleteRequestApi, getAllRequestsApi, getRequestsByIdApi } from "../../../services/requestApi"
 import { setRequestsData, setRequestData } from "../../../store/Slices/requestsSlice"
+import { createToast } from "../../Notifications/Notifications"
 
 export const getAllRequests = async (dispatch: any)=>{
     try{
@@ -30,3 +32,40 @@ export const getDeceasedByRequestId = async (dispatch: any, id:string)=>{
         console.log(err)
     }
 }
+
+//------------------HANDLE DELETE----------------
+
+export const handleDeleteRequest = (id:string, router: any)=>{
+    Swal.fire({
+        title: "Eliminar Ingreso",
+        text: "¿Esta seguro que desea eliminar el ingreso?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si",
+        cancelButtonText: "Cancelar",
+        confirmButtonColor: "#43815A",
+        cancelButtonColor: "#b32020",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          try {
+            const response = await deleteRequestApi(id);
+            console.log(response)
+            if (response.data) {
+              createToast("success","Se elimino correctamente");
+              router.push('/requests')
+            } else {
+            createToast("warning","No se pudo eliminar, intentente nuevamente");
+            }
+        } catch (error) {
+            createToast("warning","ocurrio un error, vuelva a intentar");
+            console.error(error);
+        }
+            
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+            createToast("warning","No se elimino la solicitud");
+        }
+      });
+    }
