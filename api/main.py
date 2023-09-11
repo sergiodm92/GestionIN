3f793cd0c3bf176bfe429c958117874272f22e79
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2023-09-10 23:54:12 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -22,13 +22,23 @@ app = FastAPI(
     },
 )
 
-# Configurar CORS
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "https://gestion-x10w29o04-sergiodm92.vercel.app",
-    "https://gestion-in.vercel.app"
-]
+# # Configurar CORS
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+#     "https://gestion-x10w29o04-sergiodm92.vercel.app",
+#     "https://gestion-in.vercel.app"
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+# Configurar CORS para permitir cualquier origen
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
