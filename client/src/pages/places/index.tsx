dd88b5d47c7412f3fdfe532b37e48e941b0f4536
Mariana Rocío Rionda <97262059+MariRionda@users.noreply.github.com>
2023-11-17 13:08:44 -0300

    cargar place

--- client/src/pages/places/index.tsx
+++ client/src/pages/places/index.tsx
@@ -1,41 +1,44 @@
 import { useEffect } from "react"
-import { LargeButton } from "../../components/Buttons"
+import { FormButton, LargeButton, SmallBtn } from "../../components/Buttons"
 import { useRouter } from "next/router"
 import styles from "./styles/places.module.css"
 import { getAllPlaces } from "../../components/functions/places"
 import { useAppDispatch, useAppSelector } from "../../store/hooks"
 import { getplace } from "../../store/Slices/place"
 
-const Places = ()=>{
+const Places = () => {
 
     const router = useRouter()
     const dispatch = useAppDispatch()
-    
+
     const places = useAppSelector(getplace)
 
-    useEffect(()=>{
+    useEffect(() => {
         getAllPlaces(dispatch)
-    },[])
+    }, [])
 
-    return(
+    return (
         <div className={styles.container}>
-            <div className={styles.newPlace} onClick={()=>router.push('/places/new')}>+</div>
-            {places.length?
-            (places.map((p,i)=>{
-                return(
-                    <div key={i}>
-                        <LargeButton
-                            title={p.name}
-                            onClick={()=>router.push(`/stock/${p.name}`)}
-                        />
-                    </div>
-                )
-            })):null}
+            <div className={styles.newPlace} ><SmallBtn
+                title={"Agregar deposito"}
+                onClick={() => router.push('/places/new')}
+            /></div>
+            {places.length ?
+                (places.map((p, i) => {
+                    return (
+                        <div key={i}>
+                            <LargeButton
+                                title={p.name}
+                                onClick={() => router.push(`/stock/${p.name}`)}
+                            />
+                        </div>
+                    )
+                })) : null}
             <LargeButton
                 title={"Stock Total"}
-                onClick={()=>router.push('/stock')}
+                onClick={() => router.push('/stock')}
             />
         </div>
-    )    
+    )
 }
 export default Places
