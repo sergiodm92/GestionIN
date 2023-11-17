9b61563c827444a33722f5b642007bddfb051649
Mariana Rocío Rionda <97262059+MariRionda@users.noreply.github.com>
2023-11-17 10:02:17 -0300

    Update arrays.tsx

--- client/src/components/arrays.tsx
+++ client/src/components/arrays.tsx
@@ -8,14 +8,17 @@ export const types = [
     {name:"Paris Arito", initials: "PA" }
 ]
 export const sizes = [
+    {name:"Angelito 60cm", initials: "A6" },
+    {name:"Angelito 80cm", initials: "A8" },
+    {name:"Angelito 100cm", initials: "A1" },
     {name:"160cm", initials: "16" },
     {name:"170cm", initials: "17" },
     {name:"180cm", initials: "18" },
     {name:"Normal", initials: "NO" },
-    {name:"Semi Extraordinario", initials: "SM" },
+    {name:"Semi-Extraordinario", initials: "SM" },
     {name:"Extraordinario", initials: "EX" },
-    {name:"Super", initials: "SU" },
-    {name:"Super Extraordinario", initials: "SE" }
+    {name:"Super Extraordinario", initials: "SU" },
+    {name:"Super Super Extraordinario", initials: "SE" }
 ]
 export const colors = [
     {name:"Roble", initials: "RO" },
