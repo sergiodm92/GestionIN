ef7fe9e5ca34a4a6d4491dd414b4f07ad69e88ce
sergiodm92 <crgiodm@gmail.com>
2023-12-02 01:43:31 -0300

    register

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,21 +1,25 @@
-import { use, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { postNewUserApi } from "../../services/userApi";
 import { createToast } from "../../components/Notifications/Notifications";
 import { FormButton } from "../../components/Buttons";
 import styles from "./styles/register.module.css";
 import Loading from "../../components/Loading/loading";
-import { getUser, setLoginData } from "../../store/Slices/userSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { getplace } from "../../store/Slices/place";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { getAllPlaces } from "../../components/functions/places";
+import { getUser } from "../../store/Slices/userSlice";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required("Usuario es requerido"),
-  password: Yup.string().required("Contraseña es requerida"),
+  password: Yup.string()
+    .required("Contraseña es requerida")
+    .matches(/^(?=.*[a-zA-Z])(?=.*\d)/, "Debe contener letras y números")
+    .min(8, "debe tener al menos 8 caracteres"),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Las contraseñas deben coincidir")
+    .oneOf([Yup.ref("password"), "null"], "Las contraseñas deben coincidir") //error en null de typescript
     .required("Confirmar contraseña es requerida"),
   place: Yup.string().required("Lugar es requerido"),
   admin: Yup.boolean().required("Administrador es requerido"),
@@ -25,9 +29,16 @@ const Register = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const places = useAppSelector(getplace);
-  console.log(places)
+  const userGlobal = useAppSelector(getUser)
 
-  const handleSubmit = async (values: any, { setSubmitting }) => {
+  useEffect(() => {
+    getAllPlaces(dispatch);
+    if(userGlobal.admin===false){
+      router.push("/")
+    }
+  }, []);
+
+  const handleSubmit = async (values: any, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
     try {
       const response = await postNewUserApi(values);
       if (response.data.status === "ok") {
@@ -106,7 +117,7 @@ const Register = () => {
             </div>
             <div>
               <div className={styles.inputText}>Lugar:</div>
-              <Field as="select" name="place" className={styles.selects}>
+              <Field as="select" name="place" className={styles.input}>
                 <option value="" disabled>
                   Seleccione un lugar
                 </option>
@@ -126,7 +137,7 @@ const Register = () => {
               <div className={styles.inputText}>¿Administrador?:</div>
               <Field
                 as="select"
-                className={styles.select}
+                className={styles.input}
                 id="admin"
                 name="admin"
               >
